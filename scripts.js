c40a63908e54d916b182771f36e4d3d53f915072
LeandroSRL <165808990+LeandroSRL@users.noreply.github.com>
2024-05-05 19:02:45 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+const mascarahidden = document.querySelector(".cliqueiMascara")
+
+
+function mostrarForm() {
+    form.style.left = "40%"
+    form.style.transform = "translatX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderForm() {
+    mascara.style.visibility = "hidden"
+    form.style.left = "-300px"
+    form.style.transform = "translatX(0)"
+
+}
